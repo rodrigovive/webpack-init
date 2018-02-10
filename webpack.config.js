1781f37f3d9b0bcd1106c7594e368914cb56c32f
Rodrigo <cherry199662@gmail.com>
2018-02-10 14:39:35 -0500

    Add extract-style

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
 const config = {
 
   entry: './src/index.js',
@@ -13,11 +15,16 @@ const config = {
         test: /\.js$/,
       },
       {
-        use: ['style-loader','css-loader'],
+        loader: ExtractTextPlugin.extract({
+          loader: 'css-loader'
+        }),
         test: /\.css$/
       }
     ]
-  }
+  },
+  plugins: [
+    new ExtractTextPlugin('style.css')
+  ]
 };
 
 module.exports = config;
